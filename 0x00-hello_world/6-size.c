fc0c551221ea5f4356eb3e82f81240912e30a5aa
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-07 18:13:18 +0300

    this is commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,11 +6,11 @@
  */
  int main(void)
  {
-printf("Size of char: %d byte(s)", sizeof(char));
-printf("Size of an int: %d byte(s)", sizeof(int));
-printf("Size of a long int: %d byte(s)", sizeof(long int)):
-printf("Size of long long int: %d byte(s)", sizeof(long long int));
-printf("Size of float: %d byte(s)", sizeof(float));
+printf("Size of char: %zu byte(s)", sizeof(char));
+printf("Size of an int: %zu byte(s)", sizeof(int));
+printf("Size of a long int: %zu byte(s)", sizeof(long int));
+printf("Size of long long int: %zu byte(s)", sizeof(long long int));
+printf("Size of float: %zu byte(s)", sizeof(float));
 return (0);
  }
  
