777fa9793880c9ca2e352c12f1d95a06d2f5acf3
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-08 13:42:35 +0300

    this is commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,9 +8,9 @@ int main(void)
 {
 char x;
 for (x = 0 ; x < 10 ; x++)
-putchar(x);
-for (x = 'a' ; x <= 'f' ; x++)
 putchar(x + 48);
+for (x = 'a' ; x <= 'f' ; x++)
+putchar(x);
 putchar('\n');
 return (0);
 }
