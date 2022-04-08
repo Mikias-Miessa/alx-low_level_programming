1913d64a0495a8f16b6f0846864d1c27e973e877
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-08 12:50:10 +0300

    this is readme

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
+/**
+ * main - Lowercase Letters!.
+(*
+ * Return: 0 on success
+ */
 int main(void)
 {
 char x;
-for(x='a' ; x<=z ; x++)
+for (x = 'a' ; x <= z ; x++)
 putchar(ch);
 putchar('\n');
 return (0);
