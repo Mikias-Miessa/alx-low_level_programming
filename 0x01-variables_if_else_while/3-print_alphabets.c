2951dd43630555a79dbc6e717e76dc46b6ceb807
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-08 11:41:11 +0300

    this is readme

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Lowercase Letters!.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+char x;
+
+for(x='a'; x<='z'; x++ )
+{
+putchar(x);
+}
+for(x='A'; x<='Z'; x++ )
+{
+putchar(y);
+putchar('\n');
+}
+return (0);
+}
