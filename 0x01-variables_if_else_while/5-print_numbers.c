5e96b43c060c824f4ff19adbf3388ff781e07f7b
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-08 13:06:49 +0300

    this is readme

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+include <stdio.h>
+/**
+ * main - Lowercase Letters!.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+int x;
+for (x = 0 ; x < 10 ; x++)
+printf("%d", x);
+printf("\n");
+return (0);
+}
