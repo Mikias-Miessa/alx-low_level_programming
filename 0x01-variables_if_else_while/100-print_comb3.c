9e01370f2584404250e88608083c16f033058dff
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-08 14:05:14 +0300

    this is commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -4,16 +4,16 @@
 (*
  * Return: 0 on success
  */
- int main(void)
- {
+int main(void)
+{
 int i;
 int j;
 for (i = 0 ; i < 9 ; i++)
 {
 for (j = 1 ; i < 10 ; j++)
 {
-putchar(i);
-putchar(j);
+putchar(i + 48);
+putchar(j + 48);
 putchar(',');
 putchar(' ');
 }
