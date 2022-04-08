1f52b75856f016878c07be4579f6b433ca43d5f9
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-08 12:54:16 +0300

    this is readme

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,13 +8,13 @@ int main(void)
 {
 char x;
 
-for(x='a'; x<='z'; x++ )
+for(x = 'a'; x <= 'z'; x++ )
 {
 putchar(x);
 }
-for(x='A'; x<='Z'; x++ )
+for(x = 'A'; x <= 'Z'; x++ )
 {
-putchar(y);
+putchar(x);
 putchar('\n');
 }
 return (0);
