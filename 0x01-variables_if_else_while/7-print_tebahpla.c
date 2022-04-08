8a6d15d2118cc49f0e63e52b5a2d8068bad37e5d
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-08 13:30:05 +0300

    this is commit

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Lowercase Letters!.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+char x;
+for (x = 'z' ; x >= 'a' ; x--)
+putchar(x);
+putchar('\n');
+return (0);
+}
