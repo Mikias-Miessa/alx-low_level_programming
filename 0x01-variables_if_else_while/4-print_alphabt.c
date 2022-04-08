3f703afc5e5e864a15cf231343405ca201537682
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-08 12:01:15 +0300

    this is readme

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Lowercase Letters!.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+char x;
+for(x='a' ; x<='z' ; x++)
+{
+if (x=='q' || x=='e')
+continue;
+putchar(x);
+}
+putchar('\n');
+return (0);
+}
