9f5c3a31f701b9b2a969ead2b31dbe619ad70705
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-08 13:39:06 +0300

    this is commit

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Lowercase Letters!.
+(*
+ * Return: 0 on success
+ */
+int main(void)
+{
+char x;
+for (x = 0 ; x < 10 ; x++)
+putchar(x);
+for (x = 'a' ; x <= 'f' ; x++)
+putchar(x);
+putchar('\n');
+return (0;);
+}
