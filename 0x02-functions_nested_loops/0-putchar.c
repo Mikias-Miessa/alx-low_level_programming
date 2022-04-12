34ad55a35e43a5f1160382babbd157ee3a1e5d77
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-12 12:50:44 +0300

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -6,14 +6,13 @@
  */
 int main(void)
 {
-    char a[9] = "_putcharx";
-    int i = 0 ;
-    while (a[i] != 'x')
-    {
-        _putchar(a[i]);
-        i++;
-    }
-   
-    _putchar('\n');
-    return (0);
+char a[9] = "_putcharx";
+int i = 0;
+while (a[i] != 'x')
+{
+_putchar(a[i]);
+i++;
+}
+_putchar('\n')
+return (0);
 }
