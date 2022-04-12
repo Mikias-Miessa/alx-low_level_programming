1f7e82518d08daa1be23fe070b5e99a0326fe230
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-12 13:01:45 +0300

    commit

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
- * main - check the code
+ * print_alphabet - print alphabets on lower case
  *
- * Return: Always 0.
+ * Return: Nothing
  */
-int main(void)
-{
-print_alphabet();
-return (0);
-}
+
 void print_alphabet(void)
 {
 char i = 'a';
 while (i <= 'z')
+{
 _putchar(i);
+}
+_putchar('\n');
 }
