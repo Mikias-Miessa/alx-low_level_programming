39bc7ffc7a24f9a04988da016b4c5705e17aae57
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-12 15:15:00 +0300

    commit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_last_digit - prints last digit.
+ *
+ *@a: input to be tested.
+ *
+ * Return: last digit on success.
+ */
+int print_last_digit(int a)
+{
+int b;
+b = a % 10;
+return (b);
+}
