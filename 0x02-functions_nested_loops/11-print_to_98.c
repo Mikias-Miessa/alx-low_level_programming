4db1a85072e1cfcacd0e4b5768577031a2e6fe91
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-12 18:43:17 +0300

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,15 +1,20 @@
 #include "main.h"
 /**
  * print_to_98 - prints integers
- * @n: Number parameter 
+ * @n: Number parameter
  * Return: Nothing.
  */
 void print_to_98(int n)
 {
 int i;
+
 for (i = n ; i <= 98 ; i++)
 {
-_putchar(i);
+if (i == 98)
+{
+_putchar(i + 48);
+}
+_putchar(i + 48);
 _putchar(',');
 _putchar(' ');
 }
