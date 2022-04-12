5268806f752e58207f2345d6fc4b6cbf66b6a98a
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-12 18:18:28 +0300

    commit

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * add - adds two integers.
- *@a character.
- *@b character.
- * Return: The sum
+ * add - adds two integers
+ * @a: Number parameter 1
+ * @b: number parameter 2
+ * Return: Sum
  */
 int add(int a, int b)
 {
