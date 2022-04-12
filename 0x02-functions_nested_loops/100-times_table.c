73afbb21d2a66d1fa3c9c97faa34debae6a64de2
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-12 22:09:47 +0300

    commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -7,41 +7,42 @@
  */
 void print_times_table(int n)
 {
-int i, j, a;
-if (n >= 0 && n <= 15)
-{
-for (i = 0; i <= n; i++)
-{
-for (j = 0; j <= n; j++)
-{
-if (j == 0)
-{
-_putchar(48);
-continue;
-}
-a = i * j;
-_putchar(',');
-_putchar(' ');
-if (a >= 10 && a < 100)
-{
-_putchar(' ');
-_putchar(a / 10 + 48);
-_putchar(a % 10 + 48);
-}
-else if (a >= 100)
-{
-_putchar((a / 100) + 48);
-_putchar(((a % 100) / 10) + 48);
-_putchar(a % 10 + 48);
-}
-else
-{
-_putchar(' ');
-_putchar(' ');
-_putchar(a + 48);
-}
-}
-_putchar('\n');
-}
-}
+	int i, j, p;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (i = 0; i <= n; i++)
+		{
+			for (j = 0; j <= n; j++)
+			{
+				if (j == 0)
+				{
+					_putchar(48);
+					continue;
+				}
+				p = i * j;
+				_putchar(',');
+				_putchar(' ');
+				if (p >= 10 && p < 100)
+				{
+					_putchar(' ');
+					_putchar(p / 10 + 48);
+					_putchar(p % 10 + 48);
+				}
+				else if (p >= 100)
+				{
+					_putchar((p / 100) + 48);
+					_putchar(((p % 100) / 10) + 48);
+					_putchar(p % 10 + 48);
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(p + 48);
+				}
+			}
+			_putchar('\n');
+		}
+	}
 }
