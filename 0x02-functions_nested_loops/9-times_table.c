b5a02ed4c103aefbfff04ca5af61a21cd59524fd
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-12 17:24:58 +0300

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -2,7 +2,7 @@
 /**
  * times_table - 9 by 9 times table
  *
- * Description: prints 9 by 9 multiplication table 
+ * Description: prints 9 by 9 multiplication table
  * Return: Nothing.
  */
 void times_table(void)
@@ -14,7 +14,7 @@ for (j = 0 ; j <= 9 ; j++)
 {
 int a;
 a = i * j;
-_putchar(a);
+_putchar(a + 48);
 }
 _putchar('\n');
 }
