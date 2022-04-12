dae86f24a47a83013c4b6b38c2eb6a27173abe46
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-12 17:56:36 +0300

    commit

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -12,21 +12,26 @@ for (i = 0 ; i <= 9 ; i++)
 {
 for (j = 0 ; j <= 9 ; j++)
 {
+if (j == 0)
+{
+_putchar(48);
+continue;}
+
 int a;
 a = i * j;
 if (a > 9)
 {
-_putchar(a / 10 + 48);
-_putchar(a % 10 + 48);
+
 _putchar(',');
 _putchar(' ');
+_putchar(a / 10 + 48);
+_putchar(a % 10 + 48);
+
 }
 else
 {
+_putchar(' ');
 _putchar(a + 48);
-_putchar(',');
-_putchar(' ');
-_putchar(' ');
 }
 }
 _putchar('\n');
