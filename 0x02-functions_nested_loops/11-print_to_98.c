906bf49d5e1ef08c992f4832ba7264ffd9aa0fa5
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-12 21:21:14 +0300

    commit

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,28 +4,23 @@
 /**
  * print_to_98 - print to 98
  * @n : number params
- * Return: 
+ * Return:0 or 1
  */
 
 void print_to_98(int n)
 {
-if (n < 98)
-{
+
 while (n < 98)
 {
 	printf("%i, ", n);
 	n++;
 }
-}
-else if (n > 98)
-{
 while (n > 98)
 {
 	printf("%i, ", n);
 	n--;
 }
-}
-else
+
 printf("98");
 putchar('\n');
 }
