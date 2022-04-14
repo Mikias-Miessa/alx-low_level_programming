f923d3f9dab63fb5d181e19a275ab0100495474f
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-14 13:53:46 +0300

    commit

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,17 +1,16 @@
 #include "main.h"
 /**
  * print_line - prints line.
- *@n: character.
- * Return: Nothing!
+ *
+ * @n: character.
+ * Return: Nothing.
  */
 void print_line(int n)
 {
 int i;
-if (n > 0)
+for (i = 0; i < n; i++)
 {
-for (i = 0 ; i <= n ; i++)
 _putchar('_');
-_putchar('\n');
 }
-_putchar(\n);
+_putchar('\n');
 }
