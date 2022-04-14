a1be93d195fe37b62d6a49ecce30f7c1fc6efd94
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-14 13:49:45 +0300

    commit

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - prints line.
+ *@n: character.
+ * Return: Nothing!
+ */
+void print_line(int n)
+{
+int i;
+if (n > 0)
+{
+for (i = 0 ; i <= n ; i++)
+_putchar('_');
+_putchar('\n');
+}
+_putchar(\n);
+}
