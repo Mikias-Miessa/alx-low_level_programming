8ff1c20debdc5f07fa267a11173a4a3628a4a5e6
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-14 12:50:04 +0300

    commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - multiplies two integers.
+ *
+ *@a: character.
+ *@b: character.
+ *
+ * Return: 0 on success
+ */
+int mul(int a, int b)
+{
+int c;
+c = a * b;
+printf("%d", c);
+return (0);
+}
