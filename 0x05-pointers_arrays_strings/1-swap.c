4fddfa9ba7660f8de09c2bcf3a82bb63eabb4ca9
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-18 16:03:22 +0300

    this is commit

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * swap_int - swap the value of two integers using pointer.
+ *@a: pointer
+ *@b: pointer
+ * Return: Nothing.
+ */
+void swap_int(int *a, int *b)
+{
+int *temp;
+*temp = *a;
+*a = *b;
+*b = *temp;
+}
