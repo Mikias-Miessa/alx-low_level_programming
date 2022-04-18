d2d779f0755701348ebd198d033ed6defacfbdbb
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-18 17:51:46 +0300

    this is commit

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * puts_half - prints half.
+ *
+ *@a: pointer.
+ *@n: integer.
+ */
+void print_array(int *a, int n);
+{
+int i;
+for (i = 0 ; i <= n ; i++ )
+{
+printf("%d, \n", a[i];)
+}
+_putchar('\n');
+}
