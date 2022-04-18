8a905e1fd1b49d12ea1e990c275dea333c35cc2a
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-18 16:35:50 +0300

    this is commit

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main"
+/**
+ * _puts - prints a string .
+ *@str: pointer
+ * Return: Nothing.
+ */
+void _puts(char *str)
+{
+while (*str != '\0')
+{
+_putchar(*Str);
+*str++;
+}
+_putchar(\n);
+}
