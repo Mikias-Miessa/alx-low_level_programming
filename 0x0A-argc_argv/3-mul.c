db4140d1723a1cdba8f2e934f52dea2c0e74838f
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-29 22:20:10 +0300

    this is commit

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -8,9 +8,14 @@
 int main(int argc, char *argv{})
 {
 int i , mul = 1;
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
 for (i = 1 ; i < argc ; i++)
 {
-mul = mul * argv[i];
+mul = mul * atoi(argv[i]);
 }
 printf("%d\n", mul);
 return (0);
