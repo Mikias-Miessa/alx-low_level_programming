e1953696a255a5ba6e21a7a64b130e26a7607e12
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-29 21:57:37 +0300

    this is commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - prints the name of the program
  * @argc: number of arguments passed to the function
@@ -6,9 +7,8 @@
  *
  * Return: always 0
  */
-int main(int argc, char *argv[])
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-
-printf("%d\n", argc - 1);
-return (0);
+	printf("%d\n", argc - 1);
+	return (0);
 }
