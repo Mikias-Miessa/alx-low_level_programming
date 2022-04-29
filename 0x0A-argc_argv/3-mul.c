16a4e6593415692c2b6e7e65c98b879c9efc4492
Mikias Miessa <mikiasmiessa@gmail.com>
2022-04-29 22:14:33 +0300

    this is commit

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+* main - prints multiplication result.
+* @argc: number of arguments passed to the function
+* @argv: argument vector of pointers to strings
+* return - 0
+*/
+int main(int argc, char *argv{})
+{
+int i , mul = 1;
+for (i = 1 ; i < argc ; i++)
+{
+mul = mul * argv[i];
+}
+printf("%d\n", mul);
+}
